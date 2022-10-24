96ebd5341147166679b47523c0407e91cb8464a1
gabriosilva <gabrio.lina@hotmail.com>
2022-10-24 01:22:25 -0300

    chore(exercise6): finalize the last exercise - week1

--- SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
+++ SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
@@ -8,28 +8,26 @@ using namespace std;
 1 -  Faça uma função que recebe uma certa medida e ajusta ela percentualmente entre dois valores mínimo e máximo e retorna esse valor
 */
 
-float converteSensor(float medida, float min, float max) {
-	float percentage;
-	percentage = (medida - min) / (max - min);
-	return percentage;
+float converteSensor(float medida, float min, float max)
+{
+  float percentage;
+  percentage = (medida - min) / (max - min);
+  return percentage;
 }
-
-
 
 // Descomente a função main abaixo para testar o exercício 1
 /*int main(){
   cout << "Teste Ex 1" << endl;
     cout << converteSensor(100,100,400) << endl;
   //valor esperado: 0
-  
+
     cout << converteSensor(400,100,400) << endl;
   //valor esperado: 1
-  
+
     cout << converteSensor(250,100,400) << endl;
   //valor esperado: 0.5
 }
 */
-
 
 // ----------------------------------------------------------
 
@@ -37,10 +35,11 @@ float converteSensor(float medida, float min, float max) {
 2 - Faça uma função que simule a leitura de um sensor lendo o valor do teclado ao final a função retorna este valor
 */
 
-int leComando(){
-    int sensorValue;
-    cin >> sensorValue;
-    return sensorValue;
+int leComando()
+{
+  int sensorValue;
+  cin >> sensorValue;
+  return sensorValue;
 }
 
 /*
@@ -59,15 +58,19 @@ int main(){
 Evite também que, por acidente, um valor seja escrito em uma área de memória fora do vetor
 */
 
-int* insereVetor(int elemento, int valorMax, int lastPos, int Numbers[]){
-	int nextPos;
-	nextPos = lastPos+1;
-	if((elemento) > valorMax){
-		printf("Erro: valorMax: %d, lastPos + 1 (indice): %d\n",valorMax,nextPos);
-	} else {
-		Numbers[nextPos] = elemento;
-	}
-	return Numbers;
+int *insereVetor(int elemento, int valorMax, int lastPos, int Numbers[])
+{
+  int nextPos;
+  nextPos = lastPos + 1;
+  if ((elemento) > valorMax)
+  {
+    printf("Erro: valorMax: %d, lastPos + 1 (indice): %d\n", valorMax, nextPos);
+  }
+  else
+  {
+    Numbers[nextPos] = elemento;
+  }
+  return Numbers;
 }
 
 /*
@@ -76,7 +79,7 @@ int main(){
   int valorMax = 10;
   int lastPos = -1;
   int Numbers[valorMax];
-  
+
   insereVetor(9, valorMax, lastPos, Numbers);
   lastPos = lastPos + 1;
 
@@ -97,7 +100,7 @@ int main(){
 
   insereVetor(3, valorMax, lastPos, Numbers);
   lastPos = lastPos + 1;
-  
+
   insereVetor(2, valorMax, lastPos, Numbers);
   lastPos = lastPos + 1;
 
@@ -109,7 +112,7 @@ int main(){
 
   insereVetor(100, valorMax, lastPos, Numbers);
   // valor esperado: mensagem de erro "Erro: valorMax: 10, lastPos + 1 (indice): 10"
-  
+
   cout << Numbers[0] << endl; // valor esperado: 9
   cout << Numbers[1] << endl; // valor esperado: 8
   cout << Numbers[2] << endl; // valor esperado: 7
@@ -130,37 +133,43 @@ int main(){
 A primeira função deve retornar a direção de maior distância ("Direita", "Esquerda", "Frente", "Tras") e a segunda função deve retornar esta maior distância.
 */
 
-string dirMaiorDist(int Vetor[]){
-	int maxPos = 0;
-	string posList[4] = {"Direita","Esquerda", "Frente","Tras"};
-	for(int i=0; i < 5; i++) {
-		if(Vetor[i] >= Vetor[maxPos]){
-			maxPos = i;
-		}
-	}
-	return posList[maxPos];
+string dirMaiorDist(int Vetor[])
+{
+  int maxPos = 0;
+  string posList[4] = {"Direita", "Esquerda", "Frente", "Tras"};
+  for (int i = 0; i < 5; i++)
+  {
+    if (Vetor[i] >= Vetor[maxPos])
+    {
+      maxPos = i;
+    }
+  }
+  return posList[maxPos];
 }
 
-int maiorDist(int Vetor[]){
-	int maxVal = 0;
-	for(int i=0; i < 5; i++) {
-		if(Vetor[i] >= maxVal){
-			maxVal = Vetor[i];
-		}
-	}
-	return maxVal;;
+int maiorDist(int Vetor[])
+{
+  int maxVal = 0;
+  for (int i = 0; i < 5; i++)
+  {
+    if (Vetor[i] >= maxVal)
+    {
+      maxVal = Vetor[i];
+    }
+  }
+  return maxVal;
+  ;
 }
-
 
 /*
 // Descomente a função main abaixo para testar o exercício 3
 int main(){
   //Considere que os valores sempre serão distintos
   //Considere valores de distância inteiros entre 0 e 100
-  
+
   // "Direita", "Esquerda", "Frente", "Tras"
   int posicoes [4] = {0, 20, 100, 50};
-  
+
   cout << dirMaiorDist(posicoes) << endl;
   //valor esperado: Frente
 
@@ -170,7 +179,7 @@ int main(){
 
   // "Direita", "Esquerda", "Frente", "Tras"
   int posicoes2 [4] = {95, 70, 80, 50};
-  
+
   cout << dirMaiorDist(posicoes2) << endl;
   //valor esperado: Direita
 
@@ -180,7 +189,7 @@ int main(){
 
   // "Direita", "Esquerda", "Frente", "Tras"
   int posicoes3 [4] = {10, 0, 50, 60};
-  
+
   cout << dirMaiorDist(posicoes3) << endl;
   //valor esperado: Tras
 
@@ -190,7 +199,7 @@ int main(){
 
   // "Direita", "Esquerda", "Frente", "Tras"
   int posicoes4 [4] = {54, 55, 30, 0};
-  
+
   cout << dirMaiorDist(posicoes4) << endl;
   //valor esperado: Esquerda
 
@@ -203,36 +212,36 @@ int main(){
 
 // 5 - Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e retorna verdadeiro ou falso
 
-
-bool continuar(){
+bool continuar()
+{
   bool keepGoing;
   cout << "Deseja continuar o mapeamento? 0 - sim, 1 - nao:";
   cin >> keepGoing;
   return keepGoing;
 }
 
-
 // Descomente a função main abaixo para testar o exercício 5
-/*
-int main(){
-  int parar = 0;
-  
-  while (parar == 0){
-    parar = continuar();
-  }
-  //A função deve perguntar: "Digite 1 para parar o mapeamento!"
-  //Se o usuário digitar 1, deve sair do loop while declarado acima
-  // Em caso contrário, continua no loop acima e pergunta novamente
 
-  return 1;
-}
-*/
+// int main()
+// {
+//   int parar = 0;
+
+//   while (parar == 0)
+//   {
+//     parar = continuar();
+//   }
+//   // A função deve perguntar: "Digite 1 para parar o mapeamento!"
+//   // Se o usuário digitar 1, deve sair do loop while declarado acima
+//   //  Em caso contrário, continua no loop acima e pergunta novamente
+
+//   return 1;
+// }
 
 // ----------------------------------------------------------
 
 /*
-6 - Escreva uma função que vai "dirigindo" virtualmente um robô e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda", "Frente", "Tras"). 
-A cada passo, ele verifica as distâncias aos objetos e vai mapeando o terreno para uma movimentação futura. 
+6 - Escreva uma função que vai "dirigindo" virtualmente um robô e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda", "Frente", "Tras").
+A cada passo, ele verifica as distâncias aos objetos e vai mapeando o terreno para uma movimentação futura.
 Ele vai armazenando estas distancias em um vetor fornecido como parâmetro e retorna a ultima posicao preenchida do vetor.
 Esta função deve ir lendo os 4 sensores até que um comando de pare seja enviado.
 Para simular os sensores e os comandos de pare, use as funções já construídas nos ítens anteriores e em um looping contínuo até que um pedido de parada seja enviado pelo usuário.
@@ -241,11 +250,113 @@ A função final deve utilizar as funções declaradas acima para ler os sensore
 
 // Defina as funções aqui
 
-/*
-// Descomente a função main abaixo para testar o exercício 4
-int main() {
-    // Escreva os testes aqui
-
-    return 0;
+void printIntArray(int arrayVector[], int arrayLength)
+{
+  for (int i = 0; i < arrayLength; i++)
+  {
+    int currElement;
+    currElement = arrayVector[i];
+    cout << currElement;
+    cout << "\n";
+  }
 }
-*/
+
+void dirige(int initialVector[4], int maxDireita, int maxEsquerda, int maxFrente, int maxTras)
+{
+  int direitaPos[maxDireita];
+  int esquerdaPos[maxEsquerda];
+  int frentePos[maxFrente];
+  int trasPos[maxTras];
+
+  int direitaLastPos = -1;
+  int esquerdaLastPos = -1;
+  int frenteLastPos = -1;
+  int trasLastPos = -1;
+
+  insereVetor(initialVector[0], maxDireita, direitaLastPos, direitaPos);
+  direitaLastPos++;
+
+  insereVetor(initialVector[1], maxEsquerda, esquerdaLastPos, esquerdaPos);
+  esquerdaLastPos++;
+
+  insereVetor(initialVector[2], maxFrente, frenteLastPos, frentePos);
+  frenteLastPos++;
+
+  insereVetor(initialVector[3], maxTras, trasLastPos, trasPos);
+  trasLastPos++;
+
+  int parar = 0;
+  while (parar == 0)
+  {
+    int auxVector[4] = {direitaPos[direitaLastPos], esquerdaPos[esquerdaLastPos], frentePos[frenteLastPos], trasPos[trasLastPos]};
+    int maxDistance;
+    int opositeDistance;
+    int stepSize;
+    string direcaoMaiorDistancia = dirMaiorDist(auxVector);
+    cout << "Insira o n° de passos para a direcao " << direcaoMaiorDistancia << "\n";
+
+    stepSize = leComando();
+    maxDistance = maiorDist(auxVector);
+    maxDistance = maxDistance - stepSize;
+
+    if (direcaoMaiorDistancia == "Direita")
+    {
+      auxVector[0] = maxDistance;
+      auxVector[1] += stepSize;
+    }
+    else if (direcaoMaiorDistancia == "Esquerda")
+    {
+      auxVector[1] = maxDistance;
+      auxVector[0] += stepSize;
+    }
+    else if (direcaoMaiorDistancia == "Frente")
+    {
+      auxVector[2] = maxDistance;
+      auxVector[3] += stepSize;
+    }
+    else if (direcaoMaiorDistancia == "Tras")
+    {
+      auxVector[3] = maxDistance;
+      auxVector[2] += stepSize;
+    }
+
+    insereVetor(auxVector[0], maxDireita, direitaLastPos, direitaPos);
+    direitaLastPos++;
+
+    insereVetor(auxVector[1], maxEsquerda, esquerdaLastPos, esquerdaPos);
+    esquerdaLastPos++;
+
+    insereVetor(auxVector[2], maxFrente, frenteLastPos, frentePos);
+    frenteLastPos++;
+
+    insereVetor(auxVector[3], maxTras, trasLastPos, trasPos);
+    trasLastPos++;
+
+    // Exibe as informações atuais nos vetores
+    printf("Direcoes Direita\n");
+    printIntArray(direitaPos, direitaLastPos + 1);
+
+    printf("Direcoes Esquerda\n");
+    printIntArray(esquerdaPos, esquerdaLastPos + 1);
+
+    printf("Direcoes Frente\n");
+    printIntArray(frentePos, frenteLastPos + 1);
+
+    printf("Direcoes Tras\n");
+    printIntArray(trasPos, trasLastPos + 1);
+
+    parar = continuar();
+  }
+
+  printf("Posicoes Finais\n #Direita:%d\n #Esquerda:%d\n #Frente:%d\n #Tras:%d\n", direitaPos[direitaLastPos], esquerdaPos[esquerdaLastPos], frentePos[frenteLastPos], trasPos[trasLastPos]);
+}
+
+// /*
+// Descomente a função main abaixo para testar o exercício 4
+int main()
+{
+  // Escreva os testes aqui
+  int initialPos[4] = {10, 10, 10, 10};
+  dirige(initialPos, 20, 20, 20, 20);
+  return 0;
+}
